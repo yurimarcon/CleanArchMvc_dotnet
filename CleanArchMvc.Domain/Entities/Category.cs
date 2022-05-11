787f49a59ac2e0f477c7dde00431679da54fc870
yurimarcon <yuri.erik.oliveira@gmail.com>
2022-05-10 23:46:21 -0300

    upload do projeto

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Category
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -0,0 +1,15 @@
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Product
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public decimal Stock { get; private set; }
+        public string Image { get; private set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
